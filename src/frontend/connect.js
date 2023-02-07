a1001990d68030e0635482672413699d0468a74c
aolrc-vinci <113455524+aolrc-vinci@users.noreply.github.com>
2023-02-07 19:42:06 +0100

    front end update

--- src/frontend/connect.js
+++ src/frontend/connect.js
@@ -1,15 +1,17 @@
-//Connecting 
+//Connecting pages
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
+import Login from './loginpage';
+import Register from './registerpage';
+import Locations from './locationpage'
 
 const App = () => {
   return (
     <Router>
       <Route exact path="/" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/location" component={Locations} />
     </Router>
   );
 };
